8a5a703b59dcaa7efe51a19fd4d0fe6382cf637b
Justin Fargnoli <justinfargnoli@gmail.com>
2020-02-06 23:59:12 -0500

    Adding Input trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,19 @@
-mod read;
 mod lex;
 mod parse;
+mod read;
 mod type_check;
 
-
+trait Input<T: Eq> {
+    fn peek(&self) -> T;
+    fn equals(&self, other: T) -> Option<T> {
+        if self.peek() == other {
+            Option::Some(self.next())
+        } else {
+            Option::None
+        }
+    }
+    fn next(&self) -> T;
+}
 
 #[cfg(test)]
 mod tests {
