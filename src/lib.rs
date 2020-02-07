17938871ed310760a103bb54116d56a05774c211
Justin Fargnoli <justinfargnoli@gmail.com>
2020-02-07 00:49:29 -0500

    Saving point for Chen's description of the Reader

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,16 @@ mod parse;
 mod read;
 mod type_check;
 
-trait Input<T: Eq> {
-    fn peek(&self) -> T;
-    fn equals(&self, other: T) -> Option<T> {
-        if self.peek() == other {
-            Option::Some(self.next())
-        } else {
-            Option::None
-        }
-    }
-    fn next(&self) -> T;
-}
+// trait Input<T: Eq> {
+//     fn peek(&self) -> Option<T>;
+//     fn equals(&self, other: T) -> Option<T> {
+//         if let self.peek() = other {
+//             Option::Some(self.next())
+//         }
+//         Option::None
+//     }
+//     fn next(&self) -> Option<T>;
+// }
 
 #[cfg(test)]
 mod tests {
--- src/read.rs
+++ src/read.rs
@@ -1,1 +1,31 @@
+// use crate::Input;
+use std::fs::File;
+use std::io::{Bytes, Read, Result};
+use std::iter::Peekable;
 
+// pub struct Reader {
+//     characters: Peekable<Bytes<File>>,
+// }
+
+pub fn build_reader(input_file: &str) -> Result<Peekable<Bytes<File>>> {
+    File::open(input_file)?.bytes().peekable()
+}
+
+// impl Reader {
+//     pub fn build(input_file: &str) -> Result<Reader> {
+//         Result::Ok(Reader {
+//             characters: File::open(input_file)?.bytes().peekable(),
+//         })
+//     }
+// }
+
+// impl Input<u8> for Reader {
+//     fn peek(&self) -> u8 {
+//         self.characters.peek()
+//     }
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
