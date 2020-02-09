5782bb2d9c8513e9ed58a3fcfed9b455d7c94387
Justin Fargnoli <justinfargnoli@gmail.com>
2020-02-08 23:02:58 -0500

    Refactoring the TokenStream so that is stores a vector of tokens

--- src/tokenize.rs
+++ src/tokenize.rs
@@ -1,5 +1,3 @@
-use crate::Type;
-
 #[derive(Debug, PartialEq)]
 pub enum Token {
     ParenLeft,
@@ -24,16 +22,71 @@ pub enum Token {
 
 #[derive(Debug)]
 pub struct TokenStream {
-    character_stream: Vec<char>,
+    stream: Vec<Token>,
     current_index: usize,
 }
 
 impl TokenStream {
     pub fn build(character_stream: Vec<char>) -> TokenStream {
         TokenStream {
-            character_stream,
+            stream: TokenStream::tokenize(character_stream),
             current_index: 0,
         }
+    }
+
+    fn tokenize(char_stream: Vec<char>) -> Vec<Token> {
+        let mut tokens = Vec::new();
+        let mut curr_index: usize = 0;
+        match TokenStream::next_char(&char_stream, &mut curr_index) {
+            '(' => tokens.push(Token::ParenLeft),
+            ')' => tokens.push(Token::ParenRight),
+            ',' => tokens.push(Token::Comma),
+            '\"' => tokens.push(Token::Quote),
+            'n' => {
+                assert_eq!(TokenStream::next_char(&char_stream, &mut curr_index), 'u');
+                assert_eq!(TokenStream::next_char(&char_stream, &mut curr_index), 'm');
+                let next_char = TokenStream::next_char(&char_stream, &mut curr_index);
+                if next_char == 'T' {
+                    tokens.push(Token::NumT);
+                } else if next_char == 'C' {
+                    tokens.push(Token::TNumC);
+                }
+                panic!();
+            }
+            'b' => {
+                assert_eq!(TokenStream::next_char(&char_stream, &mut curr_index), 'o');
+                assert_eq!(TokenStream::next_char(&char_stream, &mut curr_index), 'o');
+                assert_eq!(TokenStream::next_char(&char_stream, &mut curr_index), 'l');
+                assert_eq!(TokenStream::next_char(&char_stream, &mut curr_index), 'T');
+                tokens.push(Token::BoolT);
+            }
+            'f' => {
+                let next_char = TokenStream::next_char(&char_stream, &mut curr_index);
+                if next_char == 'u' {
+                    assert_eq!(TokenStream::next_char(&char_stream, &mut curr_index), 'n');
+                    assert_eq!(TokenStream::next_char(&char_stream, &mut curr_index), 'T');
+                    tokens.push(Token::FunT);
+                } else if next_char == 'a' {
+                    assert_eq!(TokenStream::next_char(&char_stream, &mut curr_index), 'l');
+                    assert_eq!(TokenStream::next_char(&char_stream, &mut curr_index), 's');
+                    assert_eq!(TokenStream::next_char(&char_stream, &mut curr_index), 'e');
+                    assert_eq!(TokenStream::next_char(&char_stream, &mut curr_index), 'C');
+                    tokens.push(Token::TFalse);
+                } else if next_char == 'd' {
+                    assert_eq!(TokenStream::next_char(&char_stream, &mut curr_index), 'C');
+                    tokens.push(Token::TFdC);
+                }
+                panic!()
+            }
+            _ => panic!("Your input wasn't able to be converted into a token stream."),
+        }
+        tokens
+    }
+
+    fn next_char(stream: &[char], curr_index: &mut usize) -> char {
+        let character = stream[*curr_index];
+        *curr_index += 1;
+        character
     }
 }
 
@@ -41,15 +94,7 @@ impl Iterator for TokenStream {
     type Item = Token;
 
     fn next(&mut self) -> Option<Token> {
-        let token = match self.character_stream[self.current_index] {
-            '(' => Some(Token::ParenLeft),
-            ')' => Some(Token::ParenRight),
-            ',' => Some(Token::Comma),
-            '\"' => Some(Token::Quote),
-            _ => panic!("Your input wasn't able to be converted into a token stream."),
-        };
-        self.current_index += 1;
-        token
+        unimplemented!()
     }
 }
 
@@ -59,13 +104,68 @@ mod tests {
     use crate::read;
 
     #[test]
-    fn tokenize_1() {
-        let characters = read::build("input1.txt").expect("Unable to open file");
+    fn tokenize_numC() {
+        let characters = vec!['n', 'u', 'm', 'C', '(', '2', ')'];
         let mut token_stream = TokenStream::build(characters);
 
         assert_eq!(token_stream.next(), Some(Token::TNumC));
         assert_eq!(token_stream.next(), Some(Token::ParenLeft));
         assert_eq!(token_stream.next(), Some(Token::Number(2)));
+        assert_eq!(token_stream.next(), Some(Token::ParenRight));
+    }
+
+    #[test]
+    fn tokenize_idC() {
+        let characters = vec!['i', 'd', 'C', '\"', 'a', 'b', '\"'];
+        let mut token_stream = TokenStream::build(characters);
+
+        assert_eq!(token_stream.next(), Some(Token::TIdC));
+        assert_eq!(token_stream.next(), Some(Token::Quote));
+        assert_eq!(token_stream.next(), Some(Token::ID(String::from("ab"))));
+        assert_eq!(token_stream.next(), Some(Token::Quote));
+    }
+
+    #[test]
+    fn tokenize_multC() {
+        let characters = vec![
+            'm', 'u', 'l', 't', 'C', '(', 'n', 'u', 'm', 'C', '(', '2', ')', ',', 'n', 'u', 'm',
+            'C', '(', '2', ')', ')',
+        ];
+        let mut token_stream = TokenStream::build(characters);
+
+        assert_eq!(token_stream.next(), Some(Token::TMultC));
+        assert_eq!(token_stream.next(), Some(Token::ParenLeft));
+        assert_eq!(token_stream.next(), Some(Token::TNumC));
+        assert_eq!(token_stream.next(), Some(Token::ParenLeft));
+        assert_eq!(token_stream.next(), Some(Token::Number(2)));
+        assert_eq!(token_stream.next(), Some(Token::ParenRight));
+        assert_eq!(token_stream.next(), Some(Token::Comma));
+        assert_eq!(token_stream.next(), Some(Token::TNumC));
+        assert_eq!(token_stream.next(), Some(Token::ParenLeft));
+        assert_eq!(token_stream.next(), Some(Token::Number(2)));
+        assert_eq!(token_stream.next(), Some(Token::ParenRight));
+        assert_eq!(token_stream.next(), Some(Token::ParenRight));
+    }
+
+    #[test]
+    fn tokenize_plusC() {
+        let characters = vec![
+            'p', 'l', 'u', 's', 'C', '(', 'n', 'u', 'm', 'C', '(', '2', ')', ',', 'n', 'u', 'm',
+            'C', '(', '2', ')', ')',
+        ];
+        let mut token_stream = TokenStream::build(characters);
+
+        assert_eq!(token_stream.next(), Some(Token::TPlusC));
+        assert_eq!(token_stream.next(), Some(Token::ParenLeft));
+        assert_eq!(token_stream.next(), Some(Token::TNumC));
+        assert_eq!(token_stream.next(), Some(Token::ParenLeft));
+        assert_eq!(token_stream.next(), Some(Token::Number(2)));
+        assert_eq!(token_stream.next(), Some(Token::ParenRight));
+        assert_eq!(token_stream.next(), Some(Token::Comma));
+        assert_eq!(token_stream.next(), Some(Token::TNumC));
+        assert_eq!(token_stream.next(), Some(Token::ParenLeft));
+        assert_eq!(token_stream.next(), Some(Token::Number(2)));
+        assert_eq!(token_stream.next(), Some(Token::ParenRight));
         assert_eq!(token_stream.next(), Some(Token::ParenRight));
     }
 }
