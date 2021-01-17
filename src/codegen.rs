a5c3a4298a8cca0fbd98a45a039c4ef0d85b2f1a
justinfargnoli <justinfargnoli@gmail.com>
2021-01-17 16:35:45 -0500

    Codegen function prototypes to 'inkwell::types::FunctionType'

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,5 +1,5 @@
 use crate::{
-    parse::{AppCStruct, FdCStruct, RecCStruct, AST},
+    parse::{FdCStruct, AST},
     type_check::Type,
 };
 use inkwell::{
@@ -7,8 +7,8 @@ use inkwell::{
     context::Context,
     module::Module,
     support::LLVMString,
-    types::BasicTypeEnum,
-    values::{BasicValueEnum, CallSiteValue, FunctionValue, IntValue},
+    types::{BasicType, FunctionType},
+    values::FunctionValue,
     AddressSpace,
 };
 
@@ -19,7 +19,7 @@ pub fn run(ast: &AST) -> Result<(), LLVMString> {
 pub struct CodeGen<'ctx> {
     context: &'ctx Context,
     module: Module<'ctx>,
-    builder: Builder<'ctx>,
+    _builder: Builder<'ctx>,
 }
 
 impl<'ctx> CodeGen<'ctx> {
@@ -28,7 +28,7 @@ impl<'ctx> CodeGen<'ctx> {
         let mut codegen = CodeGen {
             context: &context,
             module: context.create_module("main"),
-            builder: context.create_builder(),
+            _builder: context.create_builder(),
         };
 
         codegen.module(ast);
@@ -49,151 +49,79 @@ impl<'ctx> CodeGen<'ctx> {
                 }
             }
             AST::IdC(variable_name) => panic!("IdC cannot find variable {}", variable_name),
-            AST::AppC(app_fn) => {
-                self.function_application(app_fn);
-            }
+            // AST::AppC(app_fn) => {
+            //     self.function_application(app_fn);
+            // }
             AST::FdC(def_fn) => {
                 self.function_definition(def_fn);
             }
-            AST::RecC(rec_fn) => {
-                self.recursive_function(rec_fn);
-            }
+            // AST::RecC(rec_fn) => {
+            //     self.recursive_function(rec_fn);
+            // }
             _ => unimplemented!(),
         }
     }
 
-    pub fn function(&mut self, _ast: &AST) -> FunctionValue<'ctx> {
-        unimplemented!()
+    // pub fn function(&mut self, _ast: &AST) -> FunctionValue<'ctx> {
+    //     unimplemented!()
+    // }
+
+    // pub fn number(&mut self, _ast: &AST) -> IntValue<'ctx> {
+    //     unimplemented!()
+    // }
+
+    // pub fn identifier(&mut self, _ast: &AST) -> IntValue<'ctx> {
+    //     unimplemented!()
+    // }
+
+    pub fn type_to_llvm_basic_type(&self, t: &Type) -> Box<dyn BasicType<'ctx> + 'ctx> {
+        match t {
+            Type::NumT => Box::new(self.context.i64_type()),
+            Type::BoolT => Box::new(self.context.i8_type()),
+            Type::FunT { arg, ret } => Box::new(
+                self.function_prototype(arg, ret)
+                    .ptr_type(AddressSpace::Generic),
+            ),
+        }
     }
 
-    pub fn number(&mut self, _ast: &AST) -> IntValue<'ctx> {
-        unimplemented!()
-    }
-
-    pub fn identifier(&mut self, _ast: &AST) -> IntValue<'ctx> {
-        unimplemented!()
+    pub fn function_prototype(&self, ret: &Type, arg: &Type) -> FunctionType<'ctx> {
+        match ret {
+            Type::NumT => self.context.i64_type().fn_type(
+                &[self.type_to_llvm_basic_type(arg).as_basic_type_enum()],
+                false,
+            ),
+            Type::BoolT => self.context.i8_type().fn_type(
+                &[self.type_to_llvm_basic_type(arg).as_basic_type_enum()],
+                false,
+            ),
+            Type::FunT { .. } => self.type_to_llvm_basic_type(ret).fn_type(
+                &[self.type_to_llvm_basic_type(arg).as_basic_type_enum()],
+                false,
+            ),
+        }
     }
 
     pub fn function_definition(&mut self, def_fn: &FdCStruct) -> FunctionValue<'ctx> {
-        let llvm_ret_type = match def_fn.ret_type {
-            Type::NumT => self.context.i64_type(),
-            Type::BoolT => self.context.i8_type(),
-            Type::FunT { .. } => unimplemented!(),
-        };
-        let llvm_arg_type = match def_fn.arg_type {
-            Type::NumT => BasicTypeEnum::IntType(self.context.i64_type()),
-            Type::BoolT => BasicTypeEnum::IntType(self.context.i8_type()),
-            Type::FunT { .. } => {
-                BasicTypeEnum::PointerType(self.context.i64_type().ptr_type(AddressSpace::Generic))
-            }
-        };
-        let _function_value =
-            self.module
-                .add_function("", llvm_ret_type.fn_type(&[llvm_arg_type], false), None);
+        let _function_value = self.module.add_function(
+            "",
+            self.function_prototype(&def_fn.ret_type, &def_fn.arg_type),
+            None,
+        );
         unimplemented!()
     }
 
-    pub fn function_application(&mut self, app_fn: &AppCStruct) -> CallSiteValue<'ctx> {
-        let function_value = self.function(&*app_fn.func);
-        let argument = self.number(&*app_fn.arg);
-        self.builder.build_call(
-            function_value,
-            &[BasicValueEnum::IntValue(argument)],
-            "call",
-        )
-    }
+    // pub fn function_application(&mut self, app_fn: &AppCStruct) -> CallSiteValue<'ctx> {
+    //     let function_value = self.function(&*app_fn.func);
+    //     let argument = self.number(&*app_fn.arg);
+    //     self.builder.build_call(
+    //         function_value,
+    //         &[BasicValueEnum::IntValue(argument)],
+    //         "call",
+    //     )
+    // }
 
-    pub fn recursive_function<'a>(&self, _rec_fn: &RecCStruct) -> FunctionValue<'ctx> {
-        unimplemented!()
-    }
-
-    pub fn ret_type_to_llvm_type(&self, t: Type) -> BasicTypeEnum {
-        match t {
-            Type::NumT => BasicTypeEnum::IntType(self.context.i64_type()),
-            Type::BoolT => BasicTypeEnum::IntType(self.context.i8_type()),
-            Type::FunT { arg: _, ret } => self.ret_type_to_llvm_type(*ret),
-        }
-    }
+    // pub fn recursive_function<'a>(&self, _rec_fn: &RecCStruct) -> FunctionValue<'ctx> {
+    //     unimplemented!()
+    // }
 }
-
-// pub fn module<'ctx: 'a, 'a>(codegen: &'a mut CodeGen<'ctx>, ast: &AST) {
-//     codegen
-//         .module
-//         .add_function("main", codegen.context.i64_type().fn_type(&[], false), None);
-
-//     match ast {
-//         AST::NumC(num) => {
-//             if *num < 0 {
-//                 panic!("Cannot codegen negative number: {}, num");
-//             } else {
-//                 codegen.context.i64_type().const_int(*num as u64, false);
-//             }
-//         }
-//         AST::IdC(variable_name) => panic!("IdC cannot find variable {}", variable_name),
-//         AST::AppC(app_fn) => {
-//             function_application(codegen, app_fn);
-//         }
-//         AST::FdC(def_fn) => {
-//             function_definition(codegen, def_fn);
-//         }
-//         AST::RecC(rec_fn) => {
-//             recursive_function(codegen, rec_fn);
-//         }
-//         _ => unimplemented!(),
-//     }
-// }
-
-// pub fn function<'ctx: 'a, 'a>(_codegen: &'a mut CodeGen<'ctx>, _ast: &AST) -> FunctionValue<'ctx> {
-//     unimplemented!()
-// }
-
-// pub fn number<'ctx: 'a, 'a>(_codegen: &'a mut CodeGen<'ctx>, _ast: &AST) -> IntValue<'ctx> {
-//     unimplemented!()
-// }
-
-// pub fn identifier<'ctx: 'a, 'a>(_codegen: &'a mut CodeGen<'ctx>, _ast: &AST) -> IntValue<'ctx> {
-//     unimplemented!()
-// }
-
-// pub fn function_definition<'ctx: 'a, 'a>(
-//     codegen: &'a mut CodeGen<'ctx>,
-//     def_fn: &FdCStruct,
-// ) -> FunctionValue<'ctx> {
-//     let llvm_ret_type = match def_fn.ret_type {
-//         Type::NumT => codegen.context.i64_type(),
-//         Type::BoolT => codegen.context.i8_type(),
-//         Type::FunT { .. } => unimplemented!(),
-//     };
-//     let llvm_arg_type = match def_fn.arg_type {
-//         Type::NumT => BasicTypeEnum::IntType(codegen.context.i64_type()),
-//         Type::BoolT => BasicTypeEnum::IntType(codegen.context.i8_type()),
-//         Type::FunT { .. } => {
-//             BasicTypeEnum::PointerType(codegen.context.i64_type().ptr_type(AddressSpace::Generic))
-//         }
-//     };
-//     let function_value =
-//         codegen
-//             .module
-//             .add_function("", llvm_ret_type.fn_type(&[llvm_arg_type], false), None);
-//     unimplemented!()
-// }
-
-// pub fn function_application<'ctx: 'a, 'a>(
-//     codegen: &'a mut CodeGen<'ctx>,
-//     app_fn: &AppCStruct,
-// ) -> CallSiteValue<'ctx> {
-//     let function_value = function(codegen, &*app_fn.func);
-//     let argument = number(codegen, &*app_fn.arg);
-//     codegen.builder.build_call(
-//         function_value,
-//         &[BasicValueEnum::IntValue(argument)],
-//         "call",
-//     )
-// }
-
-// pub fn recursive_function<'ctx: 'a, 'a>(
-//     _codegen: &'a mut CodeGen<'ctx>,
-//     _rec_fn: &RecCStruct,
-// ) -> FunctionValue<'ctx> {
-//     unimplemented!()
-// }
